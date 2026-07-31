4d0643d3fcceab586f5cefc90848bb8a89a13804
Akinnimi Stefan <akinnimiemmanuel@gmail.com>
2026-07-31 21:19:03 +0100

    Moved JSON File to database folder

--- config.py
+++ config.py
@@ -2,5 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-BOOKS_FILE = os.path.join(BASE_DIR, "books.json")
+DATABASE_DIR = os.path.join(BASE_DIR, "database")
+
+BOOKS_FILE = os.path.join(DATABASE_DIR, "books.json")
 
